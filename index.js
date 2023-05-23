f0b577842f0dff73cf0df0082aa07a72e0757b8c
AsumVictor <iamasum369@gmail.com>
2023-05-23 13:15:35 +0000

    Fix linters errors

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
-//call the html selectors
+// call the html selectors
 const menuToggler = document.getElementById('humberger');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuClose = document.getElementById('humberger-close');
 const menuLinks = document.querySelectorAll('#mobile-nav li a');
 
-
-//Add event-listener tot the hamburger
+// Add event-listener tot the hamburger
 menuToggler.addEventListener('click', () => {
   mobileMenu.classList.add('openMenu');
   document.body.style.overflow = 'hidden';
 });
 
-
-//Close event=listener
+// Close event=listener
 menuClose.addEventListener('click', () => {
   mobileMenu.classList.remove('openMenu');
   document.body.style.overflow = 'scroll';
 });
 
-
-//Link each section of the paage
+// Link each section of the paage
 menuLinks.forEach((menuLink) => {
   menuLink.addEventListener('click', () => {
     mobileMenu.classList.remove('openMenu');
